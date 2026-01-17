96e20dcb279ecc23c9739bf245a5108f6f7d46d7
Jefferson de Paula <seu.email.aqui@exemplo.com>
2026-01-17 00:12:32 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // O "ln" garante a quebra de linha
+        System.out.println("Production System: ONLINE");
+        System.out.println("Line Efficiency: 100%");
+    }
+}
