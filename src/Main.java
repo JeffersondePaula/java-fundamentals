7014cceb051d00d45f675b890a6973c9e3e55d52
Jefferson de Paula <seu.email.aqui@exemplo.com>
2026-01-17 00:31:13 -0300

    feat: add inventory calculation logic

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
-    public static void main(String[] args) {
-        // O "ln" garante a quebra de linha
-        System.out.println("Production System: ONLINE");
-        System.out.println("Line Efficiency: 100%");
-    }
+        public static void main(String[] args) {
+            // 1. Declarando as variaveis (Criando os containers)
+            String productName = "Ajinomoto MSG Pack";
+            int quantity = 5000;
+            double weightPerUnit = 0.5; // Em Java usamos PONTO, não virgula
+
+            // 2. Processamento (Cálculo simples)
+            double totalWeigth = quantity * weightPerUnit;
+
+            // 3. Saída (Mostrando no painel)
+            System.out.println("Product: " + productName);
+            System.out.println("Total Batch Weight: " + totalWeigth + " kg");
+        }
 }
